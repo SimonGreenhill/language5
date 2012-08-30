2541cf4f0e4d33336478fcbb556ff7aaa073236f
Simon Greenhill <simon@simon.net.nz>
2012-08-31 09:50:36 +1000

    adding sitemap

---
+++ website/website/sitemap.py
@@ -0,0 +1,23 @@
+from django.contrib.sitemaps import Sitemap
+from core.models import Language, Family
+
+class LanguageSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+
+    def items(self):
+        return Language.objects.filter()
+
+    def lastmod(self, obj):
+        return obj.added
+
+
+class FamilySitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+
+    def items(self):
+        return Family.objects.filter()
+
+    def lastmod(self, obj):
+        return obj.added
