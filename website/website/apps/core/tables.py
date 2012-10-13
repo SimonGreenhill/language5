54767ed7d70234389c35caf988048408c142d867
Simon Greenhill <simon@simon.net.nz>
2012-10-13 12:49:41 +1100

    fixing some html glitches

--- website/website/apps/core/tables.py
+++ website/website/apps/core/tables.py
@@ -3,12 +3,30 @@ from django_tables2.utils import A  # alias for Accessor
 
 from website.apps.core.models import Source, Language, Family
 
+# Note, due to the current version of django_tables2 not merging in Meta classes
+# https://github.com/bradleyayers/django-tables2/issues/85
+# The work around is to inherit class Meta in the subclasses e.g.
+# class Child(DataTable):
+#     class Meta(DataTable.Meta):
+#         pass
+#
+# another annoyance is that you can't override anything in Meta - it'll cause a 
+# NameError to be raised. The work around is this:
+#
+# class Child(DataTable):
+#     class Meta(DataTable.Meta):
+#         pass
+#     Meta.var = X
+#
+# Ugly, but it works.
+
 class DataTable(tables.Table):
     """Parent class for Datatables"""
     class Meta:
         sortable = True
         attrs = {
-            'class': "table table-striped table-bordered table-condensed"
+            'class': "table table-striped table-bordered table-condensed",
+            'summary': '',
         }
     
 
@@ -24,7 +42,8 @@ class SourceIndexTable(DataTable):
         order_by_field = 'author' # default sorting
         sequence = ('id', 'author', 'year', 'reference')
         exclude = ('editor', 'added', 'slug', 'comment')
-        
+    Meta.attrs['summary'] = 'Table of Sources'
+    
 
 class LanguageIndexTable(DataTable):
     """Language Listing"""
@@ -37,7 +56,7 @@ class LanguageIndexTable(DataTable):
         order_by_field = 'language' # default sorting
         sequence = ('id', 'isocode', 'language')
         exclude = ('editor', 'added', 'slug', 'classification', 'information')
-
+    Meta.attrs['summary'] = 'Table of Languages'
 
 class FamilyIndexTable(DataTable):
     """Family Listing"""
@@ -50,4 +69,5 @@ class FamilyIndexTable(DataTable):
         order_by_field = 'family' # default sorting
         sequence = ('id', 'family', 'count')
         exclude = ('editor', 'added', 'slug')
-
+    Meta.attrs['summary'] = 'Table of Language Families'
+    
