b6be1230a4e94afe8122dca589249d53121af45d
Simon Greenhill <simon@simon.net.nz>
2012-10-31 17:52:52 +1100

    working on styling etc

--- website/website/apps/core/tables.py
+++ website/website/apps/core/tables.py
@@ -26,7 +26,7 @@ class DataTable(tables.Table):
         sortable = True
         default = u''
         attrs = {
-            'class': "table table-striped table-bordered table-condensed",
+            'class': "table table-bordered table-condensed",
             'summary': '',
         }
     
