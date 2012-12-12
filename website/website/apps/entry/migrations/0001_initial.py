3c59d155caf612cdaaf642914f5915242e9013b9
Simon Greenhill <simon@simon.net.nz>
2012-12-13 09:26:41 +1100

    adding migrations for entry

---
+++ website/website/apps/entry/migrations/0001_initial.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Task'
+        db.create_table('entry_tasks', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('editor', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('added', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('formname', self.gf('django.db.models.fields.CharField')(max_length=255)),
+        ))
+        db.send_create_signal('entry', ['Task'])
+
+        # Adding model 'Content'
+        db.create_table('entry_contents', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('editor', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('added', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('task', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['entry.Task'])),
+            ('comment', self.gf('django.db.models.fields.TextField')()),
+            ('description', self.gf('django.db.models.fields.CharField')(max_length=64)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('done', self.gf('django.db.models.fields.BooleanField')(default=False, db_index=True)),
+        ))
+        db.send_create_signal('entry', ['Content'])
+
+        # Adding model 'Result'
+        db.create_table('entry_results', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('editor', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('added', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('task', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['entry.Task'])),
+            ('content', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['entry.Content'])),
+            ('result', self.gf('django.db.models.fields.TextField')()),
+            ('comment', self.gf('django.db.models.fields.TextField')()),
+            ('imported', self.gf('django.db.models.fields.BooleanField')(default=False, db_index=True)),
+        ))
+        db.send_create_signal('entry', ['Result'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Task'
+        db.delete_table('entry_tasks')
+
+        # Deleting model 'Content'
+        db.delete_table('entry_contents')
+
+        # Deleting model 'Result'
+        db.delete_table('entry_results')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'entry.content': {
+            'Meta': {'object_name': 'Content', 'db_table': "'entry_contents'"},
+            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'comment': ('django.db.models.fields.TextField', [], {}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'done': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'db_index': 'True'}),
+            'editor': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'task': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['entry.Task']"})
+        },
+        'entry.result': {
+            'Meta': {'object_name': 'Result', 'db_table': "'entry_results'"},
+            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'comment': ('django.db.models.fields.TextField', [], {}),
+            'content': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['entry.Content']"}),
+            'editor': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'imported': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'db_index': 'True'}),
+            'result': ('django.db.models.fields.TextField', [], {}),
+            'task': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['entry.Task']"})
+        },
+        'entry.task': {
+            'Meta': {'object_name': 'Task', 'db_table': "'entry_tasks'"},
+            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'editor': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'formname': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['entry']
