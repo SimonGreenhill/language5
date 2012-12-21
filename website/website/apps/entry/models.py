5b8df0f6fe40ccdefd3895eecb395bbaa1a277d8
Simon Greenhill <simon@simon.net.nz>
2012-12-21 20:57:53 +1300

    updating model

--- website/website/apps/entry/models.py
+++ website/website/apps/entry/models.py
@@ -5,13 +5,17 @@ from django.core.files.storage import FileSystemStorage
 
 from website.apps.core.models import TrackedModel
 
+from website.apps.entry.forms import entry_forms
+
 class Task(TrackedModel):
     """Data Entry Tasks"""
-    name = models.CharField(max_length=255,
+    name = models.CharField(max_length=255, db_index=True,
         help_text="Name of Task")
-    formname = models.CharField(max_length=255,
-        help_text="Name of Form")
+    comment = models.TextField(help_text="Comment")
     source = models.ForeignKey('core.Source', default=None, blank=True, null=True)
+    form = models.CharField(default=entry_forms[0], max_length=256, 
+            choices=entry_forms,
+            help_text="Entry Forms")
     
     def __unicode__(self):
         return self.name
@@ -27,13 +31,21 @@ class Task(TrackedModel):
 class Content(TrackedModel):
     """Task Content"""
     task = models.ForeignKey('Task')
-    comment = models.TextField(help_text="Comments")
+    taskcomment = models.TextField(blank=True, null=True,
+        help_text="Task Comment for Data Entry Person")
+    comment = models.TextField(blank=True, null=True, 
+        help_text="Comments from Data Entry Person")
     description = models.CharField(max_length=64,
         help_text="Short description (e.g. page numbers)")
     image = models.ImageField(upload_to='data/%Y-%m/',
         help_text="The Page Image")
+    # raw result data
+    result = models.TextField(default=None, blank=True, null=True,
+        help_text="Raw JSON content")
     done = models.BooleanField(default=False, db_index=True,
         help_text="Data has been entered")
+    imported = models.BooleanField(default=False, db_index=True,
+        help_text="Imported into main database or not")
     
     def __unicode__(self):
         return u'#%d. %s' % (self.task_id, self.description)
@@ -44,19 +56,3 @@ class Content(TrackedModel):
     class Meta:
         db_table = 'entry_contents'
 
-
-class Result(TrackedModel):
-    task = models.ForeignKey('Task')
-    content = models.ForeignKey('Content')
-    result = models.TextField(
-        help_text="Raw JSON content")
-    comment = models.TextField(help_text="Comments")
-    imported = models.BooleanField(default=False, db_index=True,
-        help_text="Imported into main database or not")
-        
-    def __unicode__(self):
-        return u'#Task %d. Subset %d' % (self.task_id, self.content_id)
-
-    class Meta:
-        db_table = 'entry_results'
-    
