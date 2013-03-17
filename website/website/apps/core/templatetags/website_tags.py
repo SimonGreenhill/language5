484633cf1007bc8469799703270d34fff1ca9324
Simon Greenhill <simon@simon.net.nz>
2013-03-17 17:12:23 +1300

    enhance language page

--- website/website/apps/core/templatetags/website_tags.py
+++ website/website/apps/core/templatetags/website_tags.py
@@ -3,6 +3,27 @@ from django.core.urlresolvers import resolve
 
 register = template.Library()
 
+def link_ethnologue(language):
+    """Links to the Ethnologue"""
+    if language.isocode:
+        return "http://www.ethnologue.com/language/%s" % language.isocode
+    else:
+        return ""
+        
+register.filter('link_ethnologue', link_ethnologue)
+
+
+def link_olac(language):
+    """Links to the OLAC project"""
+    if language.isocode:
+        return "http://search.language-archives.org/search.html?q=%s" % language.isocode
+    else:
+        return ""
+
+register.filter('link_olac', link_olac)
+
+
+
 def active(context, view):
     try:
         resolved = resolve(context['request'].path_info)
@@ -15,3 +36,4 @@ def active(context, view):
         return ''
 
 register.simple_tag(takes_context=True)(active)
+
