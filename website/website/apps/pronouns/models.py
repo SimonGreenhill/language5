930b5185841edc43c5808010da1f566fab229e01
Simon Greenhill <simon@simon.net.nz>
2013-04-02 13:15:41 +1100

    updating model

--- website/website/apps/pronouns/models.py
+++ website/website/apps/pronouns/models.py
@@ -3,27 +3,6 @@ from django.core.urlresolvers import reverse
 
 from website.apps.core.models import TrackedModel, Language, Source
 
-ALIGNMENT_CHOICES = (
-    ('A', 'A'),
-    ('S', 'S'),
-    ('O', 'O'),
-    ('P', 'P'),
-)
-
-PERSON_CHOICES = (
-    ('1', '1st Person'),
-    ('2', '2nd Person'),
-    ('3', '3rd Person'),
-    ('12', '12 Person'),
-)
-
-NUMBER_CHOICES = (
-    ('sg', 'Singular'),
-    ('du', 'Dual'),
-    ('pl', 'Plural'),
-    ('tr', 'Trial'),
-)
-
 
 class Paradigm(TrackedModel):
     """Paradigm Details"""
@@ -45,6 +24,27 @@ class Paradigm(TrackedModel):
 
 class Pronoun(TrackedModel):
     """Pronoun Data"""
+    ALIGNMENT_CHOICES = (
+        ('A', 'A'),
+        ('S', 'S'),
+        ('O', 'O'),
+        ('P', 'P'),
+    )
+    
+    PERSON_CHOICES = (
+        ('1', '1st Person'),
+        ('2', '2nd Person'),
+        ('3', '3rd Person'),
+        ('12', '12 Person'),
+    )
+    
+    NUMBER_CHOICES = (
+        ('sg', 'Singular'),
+        ('du', 'Dual'),
+        ('pl', 'Plural'),
+        ('tr', 'Trial'),
+    )
+    
     paradigm = models.ForeignKey('Paradigm')
     comment = models.TextField(blank=True, null=True,
         help_text="Comment on this paradigm")
@@ -66,9 +66,18 @@ class Pronoun(TrackedModel):
         
 class Relationship(TrackedModel):
     """Relationships Data"""
+    RELATIONSHIP_CHOICES = (
+        ('TD', 'Totally Distinct'),
+        ('FO', 'Formal Overlap'),
+        ('FI', 'Formal Increment'),
+        ('TS', 'Total Syncretism'),
+    )
+    
     paradigm = models.ForeignKey('Paradigm')
     pronoun1 = models.ForeignKey('Pronoun', related_name="pronoun1")
     pronoun2 = models.ForeignKey('Pronoun', related_name="pronoun2")
+    relationship = models.CharField(max_length=2, choices=RELATIONSHIP_CHOICES,
+        default=None, blank=True, null=True, help_text="Relationship")
     comment = models.TextField(blank=True, null=True,
         help_text="Comment on this paradigm")
     
