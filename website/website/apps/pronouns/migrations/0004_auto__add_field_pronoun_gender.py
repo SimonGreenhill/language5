2df4442e5db1bc7cc859c35eb27b9521d7cec33a
Simon Greenhill <simon@simon.net.nz>
2013-04-06 17:30:35 +1100

    adding gender

---
+++ website/website/apps/pronouns/migrations/0004_auto__add_field_pronoun_gender.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Pronoun.gender'
+        db.add_column('pronouns', 'gender',
+                      self.gf('django.db.models.fields.CharField')(max_length=1, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Pronoun.gender'
+        db.delete_column('pronouns', 'gender')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'core.family': {
+            'Meta': {'object_name': 'Family', 'db_table': "'families'"},
+            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'editor': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'family': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '64'})
+        },
+        u'core.language': {
+            'Meta': {'unique_together': "(('isocode', 'language'),)", 'object_name': 'Language', 'db_table': "'languages'"},
+            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'classification': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'editor': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'family': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['core.Family']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'information': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'isocode': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '3', 'null': 'True', 'blank': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64', 'db_index': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '64'})
+        },
+        u'core.source': {
+            'Meta': {'object_name': 'Source', 'db_table': "'sources'"},
+            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'bibtex': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'comment': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'editor': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'reference': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '64'}),
+            'year': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'pronouns.paradigm': {
+            'Meta': {'object_name': 'Paradigm', 'db_table': "'paradigms'"},
+            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'comment': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'editor': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Language']"}),
+            'source': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Source']"})
+        },
+        u'pronouns.pronoun': {
+            'Meta': {'object_name': 'Pronoun', 'db_table': "'pronouns'"},
+            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'alignment': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'comment': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'editor': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'form': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'gender': ('django.db.models.fields.CharField', [], {'max_length': '1', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'number': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'paradigm': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['pronouns.Paradigm']"}),
+            'person': ('django.db.models.fields.CharField', [], {'max_length': '2'})
+        },
+        u'pronouns.relationship': {
+            'Meta': {'object_name': 'Relationship', 'db_table': "'pronoun_relationships'"},
+            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'comment': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'editor': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'paradigm': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['pronouns.Paradigm']"}),
+            'pronoun1': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'pronoun1'", 'to': u"orm['pronouns.Pronoun']"}),
+            'pronoun2': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'pronoun2'", 'to': u"orm['pronouns.Pronoun']"}),
+            'relationship': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '2', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['pronouns']
--- website/website/apps/pronouns/models.py
+++ website/website/apps/pronouns/models.py
@@ -14,6 +14,20 @@ class Paradigm(TrackedModel):
     def __unicode__(self):
         return "Paradigm: %s" % self.language.slug
     
+    def save(self):
+        if not self.pk:
+            do_prefill = True
+        else:
+            do_prefill = False
+        super(Paradigm, self).save(*args, **kwargs)
+        
+        if do_prefill:
+            self._prefill_pronouns() # Prefill Pronouns
+    
+    def _prefill_pronouns(self):
+        for p, n, g, a in Pronoun._generate_all_combinations():
+            print p, n, g, a
+    
     @models.permalink
     def get_absolute_url(self):
         return ('pronoun:edit', [self.slug])
@@ -45,6 +59,12 @@ class Pronoun(TrackedModel):
        # ('tr', 'Trial'),
     )
     
+    GENDER_CHOICES = (
+        ("M", 'Masculine'),
+        ("F", 'Feminine'),
+      #  ("N", "Neuter"),
+    )
+    
     paradigm = models.ForeignKey('Paradigm')
     comment = models.TextField(blank=True, null=True,
         help_text="Comment on this paradigm")
@@ -54,11 +74,39 @@ class Pronoun(TrackedModel):
         help_text="Person")
     number = models.CharField(max_length=2, choices=NUMBER_CHOICES,
         help_text="Number")
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES,
+        blank=True, null=True,
+        help_text="Gender")
     form = models.TextField(blank=True, null=True,
         help_text="Form")
         
     def __unicode__(self):
         return '%s %s%s %s: %s' % (self.paradigm, self.person, self.number, self.alignment, self.gloss)
+    
+    @staticmethod
+    def _generate_all_combinations():
+        out = []
+        for p in Pronoun.PERSON_CHOICES:
+            for n in Pronoun.NUMBER_CHOICES:
+                for g in Pronoun.GENDER_CHOICES:
+                    for a in Pronoun.ALIGNMENT_CHOICES:
+                        if p[0] == '12' and n[0] == 'sg':
+                            continue
+                        out.append((p[0], n[0], g[0], a[0]))
+        return out
+    
+    @staticmethod
+    def _generate_all_combinations_in_rows():
+        out = {}
+        for p in Pronoun.PERSON_CHOICES:
+            out[p] = out.get(p, [])
+            for n in Pronoun.NUMBER_CHOICES:
+                for g in Pronoun.GENDER_CHOICES:
+                    for a in Pronoun.ALIGNMENT_CHOICES:
+                        if p[0] == '12' and n[0] == 'sg':
+                            continue
+                        out[p].append((p[0], n[0], g[0], a[0]))
+        return out
         
     class Meta:
         db_table = 'pronouns'
