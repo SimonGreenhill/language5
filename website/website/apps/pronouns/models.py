ab75ad6ed91c482459cc0919559c972274d2559e
Simon Greenhill <simon@simon.net.nz>
2013-04-13 16:03:49 +1000

    updates

--- website/website/apps/pronouns/models.py
+++ website/website/apps/pronouns/models.py
@@ -4,6 +4,429 @@ from django.contrib.auth.models import User
 
 from website.apps.core.models import TrackedModel, Language, Source
 
+PronounCombinations = [
+    # 1st (excl) Person, Sing.
+    {
+        'alignment': ('A', 'A'),
+        'gender': None,
+        'number': ('sg', 'Singular'),
+        'person': ('1', '1st (excl) Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': None,
+        'number': ('sg', 'Singular'),
+        'person': ('1', '1st (excl) Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': None,
+        'number': ('sg', 'Singular'),
+        'person': ('1', '1st (excl) Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': None,
+        'number': ('sg', 'Singular'),
+        'person': ('1', '1st (excl) Person')
+    },
+    # 1st (excl) Person, Dual.
+    {
+        'alignment': ('A', 'A'),
+        'gender': None,
+        'number': ('du', 'Dual'),
+        'person': ('1', '1st (excl) Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': None,
+        'number': ('du', 'Dual'),
+        'person': ('1', '1st (excl) Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': None,
+        'number': ('du', 'Dual'),
+        'person': ('1', '1st (excl) Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': None,
+        'number': ('du', 'Dual'),
+        'person': ('1', '1st (excl) Person')
+    },
+    
+    # 1st (excl) Person, Plural
+    {
+        'alignment': ('A', 'A'),
+        'gender': None,
+        'number': ('pl', 'Plural'),
+        'person': ('1', '1st (excl) Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': None,
+        'number': ('pl', 'Plural'),
+        'person': ('1', '1st (excl) Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': None,
+        'number': ('pl', 'Plural'),
+        'person': ('1', '1st (excl) Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': None,
+        'number': ('pl', 'Plural'),
+        'person': ('1', '1st (excl) Person')
+    },
+    
+    # 1st (incl) Person, Dual
+    {
+        'alignment': ('A', 'A'),
+        'gender': None,
+        'number': ('du', 'Dual'),
+        'person': ('12', '1st (incl) Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': None,
+        'number': ('du', 'Dual'),
+        'person': ('12', '1st (incl) Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': None,
+        'number': ('du', 'Dual'),
+        'person': ('12', '1st (incl) Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': None,
+        'number': ('du', 'Dual'),
+        'person': ('12', '1st (incl) Person')
+    },
+    
+    # 1st (incl) Person, Plural
+    {
+        'alignment': ('A', 'A'),
+        'gender': None,
+        'number': ('pl', 'Plural'),
+        'person': ('12', '1st (incl) Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': None,
+        'number': ('pl', 'Plural'),
+        'person': ('12', '1st (incl) Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': None,
+        'number': ('pl', 'Plural'),
+        'person': ('12', '1st (incl) Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': None,
+        'number': ('pl', 'Plural'),
+        'person': ('12', '1st (incl) Person')
+    },
+    
+    # 2nd person Sg.
+    {
+        'alignment': ('A', 'A'),
+        'gender': None,
+        'number': ('sg', 'Singular'),
+        'person': ('2', '2nd Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': None,
+        'number': ('sg', 'Singular'),
+        'person': ('2', '2nd Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': None,
+        'number': ('sg', 'Singular'),
+        'person': ('2', '2nd Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': None,
+        'number': ('sg', 'Singular'),
+        'person': ('2', '2nd Person')
+    },
+    # 2nd Person Dual. 
+    {
+        'alignment': ('A', 'A'),
+        'gender': None,
+        'number': ('du', 'Dual'),
+        'person': ('2', '2nd Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': None,
+        'number': ('du', 'Dual'),
+        'person': ('2', '2nd Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': None,
+        'number': ('du', 'Dual'),
+        'person': ('2', '2nd Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': None,
+        'number': ('du', 'Dual'),
+        'person': ('2', '2nd Person')
+    },
+    # 2nd Person, Plural
+    {
+        'alignment': ('A', 'A'),
+        'gender': None,
+        'number': ('pl', 'Plural'),
+        'person': ('2', '2nd Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': None,
+        'number': ('pl', 'Plural'),
+        'person': ('2', '2nd Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': None,
+        'number': ('pl', 'Plural'),
+        'person': ('2', '2nd Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': None,
+        'number': ('pl', 'Plural'),
+        'person': ('2', '2nd Person')
+    },
+    # 3rd Person ----- INCLUDES GENDER
+    {
+        'alignment': ('A', 'A'),
+        'gender': ('M', 'Masculine'),
+        'number': ('sg', 'Singular'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': ('M', 'Masculine'),
+        'number': ('sg', 'Singular'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': ('M', 'Masculine'),
+        'number': ('sg', 'Singular'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': ('M', 'Masculine'),
+        'number': ('sg', 'Singular'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('A', 'A'),
+        'gender': ('F', 'Feminine'),
+        'number': ('sg', 'Singular'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': ('F', 'Feminine'),
+        'number': ('sg', 'Singular'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': ('F', 'Feminine'),
+        'number': ('sg', 'Singular'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': ('F', 'Feminine'),
+        'number': ('sg', 'Singular'),
+        'person': ('3', '3rd Person')
+    },
+    {   
+        'alignment': ('A', 'A'),
+        'gender': ('N', 'Neuter'),
+        'number': ('sg', 'Singular'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': ('N', 'Neuter'),
+        'number': ('sg', 'Singular'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': ('N', 'Neuter'),
+        'number': ('sg', 'Singular'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': ('N', 'Neuter'),
+        'number': ('sg', 'Singular'),
+        'person': ('3', '3rd Person')},
+    {
+        'alignment': ('A', 'A'),
+        'gender': ('M', 'Masculine'),
+        'number': ('du', 'Dual'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': ('M', 'Masculine'),
+        'number': ('du', 'Dual'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': ('M', 'Masculine'),
+        'number': ('du', 'Dual'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': ('M', 'Masculine'),
+        'number': ('du', 'Dual'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('A', 'A'),
+        'gender': ('F', 'Feminine'),
+        'number': ('du', 'Dual'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': ('F', 'Feminine'),
+        'number': ('du', 'Dual'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': ('F', 'Feminine'),
+        'number': ('du', 'Dual'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': ('F', 'Feminine'),
+        'number': ('du', 'Dual'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('A', 'A'),
+        'gender': ('N', 'Neuter'),
+        'number': ('du', 'Dual'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': ('N', 'Neuter'),
+        'number': ('du', 'Dual'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': ('N', 'Neuter'),
+        'number': ('du', 'Dual'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': ('N', 'Neuter'),
+        'number': ('du', 'Dual'),
+        'person': ('3', '3rd Person')},
+    {
+        'alignment': ('A', 'A'),
+        'gender': ('M', 'Masculine'),
+        'number': ('pl', 'Plural'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': ('M', 'Masculine'),
+        'number': ('pl', 'Plural'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': ('M', 'Masculine'),
+        'number': ('pl', 'Plural'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': ('M', 'Masculine'),
+        'number': ('pl', 'Plural'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('A', 'A'),
+        'gender': ('F', 'Feminine'),
+        'number': ('pl', 'Plural'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': ('F', 'Feminine'),
+        'number': ('pl', 'Plural'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': ('F', 'Feminine'),
+        'number': ('pl', 'Plural'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': ('F', 'Feminine'),
+        'number': ('pl', 'Plural'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('A', 'A'),
+        'gender': ('N', 'Neuter'),
+        'number': ('pl', 'Plural'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('S', 'S'),
+        'gender': ('N', 'Neuter'),
+        'number': ('pl', 'Plural'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('O', 'O'),
+        'gender': ('N', 'Neuter'),
+        'number': ('pl', 'Plural'),
+        'person': ('3', '3rd Person')
+    },
+    {
+        'alignment': ('P', 'Possessive'),
+        'gender': ('N', 'Neuter'),
+        'number': ('pl', 'Plural'),
+        'person': ('3', '3rd Person')
+    }
+]
+
+
 class Paradigm(TrackedModel):
     """Paradigm Details"""
     language = models.ForeignKey(Language)
@@ -26,18 +449,30 @@ class Paradigm(TrackedModel):
     
     def _prefill_pronouns(self):
         ed = User.objects.get(pk=1)
+        existing_pronouns = self.pronoun_set.all()
+        
         for comb in Pronoun._generate_all_combinations():
-            obj = Pronoun.objects.create(
-                paradigm=self,
-                person=comb['person'][0],
-                number=comb['number'][0],
-                gender=comb['gender'][0],
-                alignment=comb['alignment'][0],
-                form="",
-                #form="%s - %s - %s - %s" % (comb['person'][0], comb['number'][0], comb['gender'][0], comb['alignment'][0]) ,
-                editor=ed
-            )
-            obj.save()
+            NEEDED = True
+            gender = None if comb['gender'] is None else comb['gender'][0]
+            for e in existing_pronouns:
+                old = (e.gender, e.number, e.person, e.alignment)
+                new = (gender, comb['number'][0], comb['person'][0], comb['alignment'][0])
+                if old == new:
+                    NEEDED = False
+                    break
+            
+            if NEEDED:
+                obj = Pronoun.objects.create(
+                    editor = ed,
+                    paradigm = self,
+                    person = comb['person'][0],
+                    number = comb['number'][0],
+                    alignment = comb['alignment'][0],
+                    gender = gender,
+                    form=""
+                    #form="%s - %s - %s - %s" % (comb['person'][0], comb['number'][0], comb['gender'][0], comb['alignment'][0]) ,
+                )
+                obj.save()
     
     @models.permalink
     def get_absolute_url(self):
@@ -96,25 +531,18 @@ class Pronoun(TrackedModel):
     
     @staticmethod
     def _generate_all_combinations():
-        out = []
-        for p in Pronoun.PERSON_CHOICES:
-            for n in Pronoun.NUMBER_CHOICES:
-                for g in Pronoun.GENDER_CHOICES:
-                    for a in Pronoun.ALIGNMENT_CHOICES:
-                        if p[0] == '12' and n[0] == 'sg':
-                            continue
-                        out.append({"person": p, "number": n, "gender": g, "alignment": a})
-        return out
+        return PronounCombinations
         
     @staticmethod
     def _generate_all_rows():
-        out = []
-        for p in Pronoun.PERSON_CHOICES:
-            for n in Pronoun.NUMBER_CHOICES:
-                for g in Pronoun.GENDER_CHOICES:
-                    if p[0] == '12' and n[0] == 'sg':
-                        continue
-                    out.append({"person": p, "number": n, "gender": g,})
+        out, seen = [], []
+        # Each combination of gender, number and person
+        for p in Pronoun._generate_all_combinations():
+            p2 = p.copy()
+            del(p2['alignment'])
+            if p2 not in seen:
+                out.append(p2)
+                seen.append(p2)
         return out
 
     
--- website/website/apps/pronouns/tools.py
+++ website/website/apps/pronouns/tools.py
@@ -1,12 +1,24 @@
 from website.apps.pronouns.models import Pronoun
 
-def repr_row_instance(p):
-    return "%s %s %s" % (p.get_person_display(), p.get_number_display(), p.get_gender_display())
+def repr_row(p):
+    # handle objects
+    if isinstance(p, Pronoun):
+        if p.gender is None:
+            return " ".join([p.get_person_display(), p.get_number_display()])
+        else:
+            return " ".join([p.get_person_display(), p.get_number_display(), p.get_gender_display()])
+    # handle dictionary
+    else:
+        if p['gender'] is None:
+            return " ".join([p['person'][1], p['number'][1]])
+        else:
+            return " ".join([p['person'][1], p['number'][1], p['gender'][1]])
 
+    
 def add_pronoun_ordering(pronoun_form):
     rows = {}
     for form in pronoun_form:
-        row = repr_row_instance(form.instance)
+        row = repr_row(form.instance)
         rows[row] = rows.get(row, 
             dict(zip([x[0] for x in Pronoun.ALIGNMENT_CHOICES], [None for x in Pronoun.ALIGNMENT_CHOICES]))
         )
@@ -15,7 +27,7 @@ def add_pronoun_ordering(pronoun_form):
     pronoun_form.pronoun_rows = []
     # Sort
     for row in Pronoun._generate_all_rows():
-        wanted_label = "%s %s %s" % (row['person'][1], row['number'][1], row['gender'][1]) 
+        wanted_label = repr_row(row)
         found_row = False
         for label in rows:
             if wanted_label == label:
