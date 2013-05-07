96701d6990704f1bd161fe0170c016475541563e
Simon Greenhill <simon@simon.net.nz>
2013-05-07 10:13:06 +1000

    adding iota to charpicker

--- website/static/js/charpicker.js
+++ website/static/js/charpicker.js
@@ -1,7 +1,7 @@
 $(function () {
     $('#picker').keypad({target: $(':input:first'), 
     // layout: ['àáâãäåæçèéêë', 'ìííîïñòóôõöø', 'ðþùúûüýÿ' + $.keypad.ENTER + $.keypad.SHIFT]});
-    layout: ['àáâãäåạɐæʌèéêëɛəìííîïɨịñŋòóôõöøðɔþùúûüụųʔřɬɤƀꝑšʷᵘ·' + $.keypad.ENTER + $.keypad.SHIFT]});
+    layout: ['àáâãäåạɐæʌèéêëɛəìííîïɨịɩñŋòóôõöøðɔþùúûüụųʔřɬɤƀꝑšʷᵘ·' + $.keypad.ENTER + $.keypad.SHIFT]});
     var keypadTarget = null; 
     $(':input').focus(function() { 
     if (keypadTarget != this) { 
