31ff94583944d0f06f3621f7a9eb9f2022ac07ba
Simon Greenhill <simon@simon.net.nz>
2013-05-08 11:16:37 +1000

    adding simple filter for checkpoint to entry.admin

--- website/website/apps/entry/admin.py
+++ website/website/apps/entry/admin.py
@@ -3,10 +3,40 @@ from reversion.admin import VersionAdmin
 from website.apps.entry.models import Task, TaskLog
 from website.apps.core.admin import TrackedModelAdmin
 
+class CheckpointListFilter(admin.SimpleListFilter):
+    title = 'Has Checkpoint'
+    
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'has_checkpoint'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (
+            ('yes', 'Has Checkpoint'),
+            ('no', 'No Checkpoint'),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        if self.value() == 'yes':
+            return queryset.filter(checkpoint__isnull=False).exclude(checkpoint__iexact='')
+        if self.value() == 'no':
+            return queryset.filter(checkpoint__isnull=True).filter(checkpoint__exact='')
+
 class TaskAdmin(TrackedModelAdmin, VersionAdmin):
     date_hierarchy = 'added'
     list_display = ('name', 'source', 'language', 'records', 'view', 'added', 'completable', 'done')
-    list_filter = ('editor', 'source', 'language', 'view', 'done')
+    list_filter = ('editor', 'done', 'completable', CheckpointListFilter, 'source', 'language', 'view')
     ordering = ('name',)
 
 
