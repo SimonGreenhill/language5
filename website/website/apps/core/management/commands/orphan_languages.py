6ab0c97a8d05b517daa4be38f8067c1f49924ed8
Simon Greenhill <simon@simon.net.nz>
2013-06-17 11:31:57 +1000

    updating orphan script

--- website/website/apps/core/management/commands/orphan_languages.py
+++ website/website/apps/core/management/commands/orphan_languages.py
@@ -1,15 +1,39 @@
 # -*- coding: utf-8 -*-
+from django.db.models import Count
 from optparse import make_option
 from django.core.management.base import BaseCommand
 
-from website.apps.core.models import Language, Family
+from website.apps.core.models import Language, Family, Source
+from website.apps.lexicon.models import Word
 
 class Command(BaseCommand):
     args = ''
-    help = 'lists the languages who do not belong to a language'
+    help = 'lists the orphaned data'
     output_transaction = True
     
     def handle(self, *args, **options):
+        print 'Languages not in a family:'
         for o in Language.objects.all().order_by('slug'):
             if len(o.family.all()) == 0:
-                print o
+                print " - ", o.slug
+        print
+        
+        print 'Languages with zero entries:'
+        for o in Language.objects.annotate(count=Count('lexicon')).all().order_by('slug'):
+            if o.count == 0:
+                print " - ", o.slug
+        print
+        
+        print 'Sources with zero entries:'
+        for o in Source.objects.annotate(count=Count('lexicon')).all().order_by('slug'):
+            if o.count == 0:
+                print " - ", o.slug
+        print
+
+        print 'Words with zero entries:'
+        for o in Word.objects.annotate(count=Count('lexicon')).all().order_by('slug'):
+            if o.count == 0:
+                print " - ", o.slug
+        print
+        
+        
