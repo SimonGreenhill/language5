36421ad8ea3d9689f3d5a2857e59b7c1fc1792d5
Simon Greenhill <simon@simon.net.nz>
2013-06-20 09:44:26 +1000

    fixing commit error

--- website/static/js/charpicker.js
+++ website/static/js/charpicker.js
@@ -1,11 +1,7 @@
 $(function () {
     $('#picker').keypad({target: $(':input:first'), 
     // layout: ['àáâãäåæçèéêë', 'ìííîïñòóôõöø', 'ðþùúûüýÿ' + $.keypad.ENTER + $.keypad.SHIFT]});
-<<<<<<< local
-    layout: ['àáâãäåæçèéêëəìííîïñŋòóôõöøðþùúûüʔřɬ' + $.keypad.ENTER + $.keypad.SHIFT]});
-=======
     layout: ['àáâãäåạɐæʌèéêëɛəìííîïɨịɩñŋòóôõöøðɔþùúûüụųʔřɬɤƀꝑšʷᵘ·' + $.keypad.ENTER + $.keypad.SHIFT]});
->>>>>>> other
     var keypadTarget = null; 
     $(':input').focus(function() { 
     if (keypadTarget != this) { 
