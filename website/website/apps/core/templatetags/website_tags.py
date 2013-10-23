b844324ce329c9a58e31197cee8fd5e574298b13
Simon Greenhill <simon@simon.net.nz>
2013-10-23 11:05:56 +1100

    adding link to glottolog

--- website/website/apps/core/templatetags/website_tags.py
+++ website/website/apps/core/templatetags/website_tags.py
@@ -1,53 +1,63 @@
 from django import template
 from django.core.urlresolvers import resolve
-from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
-
-from ifinstalled import do_ifinstalled
+from website.apps.core.models import Language
+from website.apps.core.templatetags.ifinstalled import do_ifinstalled
 
 register = template.Library()
 
 @register.filter
-def link_ethnologue(language):
+def link_ethnologue(lang):
     """Links to the Ethnologue"""
-    if language.isocode:
-        return "http://www.ethnologue.com/language/%s" % language.isocode
+    if isinstance(lang, Language) and lang.isocode:
+        return "http://www.ethnologue.com/language/%s" % lang.isocode
     else:
         return ""
 
 @register.filter
-def link_olac(language):
+def link_olac(lang):
     """Links to the OLAC project"""
-    if language.isocode:
-        return "http://search.language-archives.org/search.html?q=%s" % language.isocode
+    if isinstance(lang, Language) and lang.isocode:
+        return "http://search.language-archives.org/search.html?q=%s" % lang.isocode
     else:
         return ""
 
 @register.filter
-def link_llmap(language):
-    if language.isocode:
-        return "http://llmap.org/languages/%s.html" % language.isocode
+def link_llmap(lang):
+    """Links to LLMap"""
+    if isinstance(lang, Language) and lang.isocode:
+        return "http://llmap.org/languages/%s.html" % lang.isocode
     else:
         return ""
 
 @register.filter
-def link_multitree(language):
-    if language.isocode:
-        return "http://multitree.org/codes/%s" % language.isocode
+def link_multitree(lang):
+    """Links to MultiTree"""
+    if isinstance(lang, Language) and lang.isocode:
+        return "http://multitree.org/codes/%s" % lang.isocode
     else:
         return ""
 
 @register.filter
-def language_map(language):
+def link_glottolog(lang):
+    """Links to Glottolog"""
+    if isinstance(lang, Language) and lang.isocode:
+        return "http://glottolog.org/resource/languoid/iso/%s" % lang.isocode
+    else:
+        return ""
+
+
+@register.filter
+def lang_map(lang):
     """Embeds a link to LLMap"""
     WIDTH = 400
     HEIGHT = 300
     
-    if language.isocode:
+    if isinstance(lang, Language) and lang.isocode:
         return mark_safe("""
         <img src="http://llmap.org/language/%s.png?width=%d&height=%d" alt="Map of %s: courtesy of LL-MAP" />
-        """ % (language.isocode, WIDTH, HEIGHT, unicode(language))
-        )
+        """ % (lang.isocode, WIDTH, HEIGHT, unicode(lang))
+        ).strip()
     else:
         return ""
 
