43230c246963ea8a366bd89d327b7bacea3ff436
Simon Greenhill <simon@simon.net.nz>
2013-11-19 13:05:09 +1100

    adding analect type to pronoun system

--- website/website/apps/pronouns/forms.py
+++ website/website/apps/pronouns/forms.py
@@ -13,7 +13,7 @@ from website.apps.pronouns.tools import full_repr_row
 class ParadigmForm(forms.ModelForm):
     class Meta:
         model = Paradigm
-        fields = ['language', 'source', 'comment']
+        fields = ['language', 'source', 'analect', 'comment']
         widgets = {
             'comment': forms.widgets.Textarea(attrs={'cols':60, 'rows':5, 'class': 'field span12'}),
         }
