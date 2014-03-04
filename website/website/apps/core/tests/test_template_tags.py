183305d860df9482c87e603a0790d3850060c3f2
Simon Greenhill <simon@simon.net.nz>
2014-03-04 16:41:09 -0500

    adding wikipedia links

--- website/website/apps/core/tests/test_template_tags.py
+++ website/website/apps/core/tests/test_template_tags.py
@@ -5,97 +5,70 @@ from website.apps.core.templatetags.website_tags import link_olac
 from website.apps.core.templatetags.website_tags import link_multitree
 from website.apps.core.templatetags.website_tags import link_glottolog
 from website.apps.core.templatetags.website_tags import link_llmap
+from website.apps.core.templatetags.website_tags import link_wikipedia
 from website.apps.core.templatetags.website_tags import language_map
 from website.apps.core.models import Language
 
 class LanguageMixin(object):
+    """
+    Mixin for testing the various linking functions
+    
+    This will test if they handle empty/null values gracefully, and return the
+    expected URL for a given isocode
+    """
+    function = None # Override in child functions
+    expected = None # Override in child functions
+    
     def setUp(self):
         self.editor = User.objects.create(username='admin')
         self.language = Language.objects.create(
                         language='A', slug='langa', 
                         information='i.1', classification='a, b',
                         isocode='aaa', editor=self.editor)
+    
+    def test_empty(self):
+        self.assertEqual(self.function(""), "")
+        
+    def test_null(self):
+        self.assertEqual(self.function(None), "")
+    
+    def test_expected(self):
+        f = self.function(self.language).strip()
+        self.assertEqual(f, self.expected)
         
 
 class Test_Link_Ethnologue(LanguageMixin, TestCase):
-    def test(self):
-        self.assertEqual(
-            link_ethnologue(self.language), 
-            "http://www.ethnologue.com/language/aaa"
-        )
-    def test_empty(self):
-        self.assertEqual(link_ethnologue(""), "")
-    
-    def test_empty2(self):
-        self.assertEqual(link_ethnologue(None), "")
+    function = staticmethod(link_ethnologue)
+    expected = "http://www.ethnologue.com/language/aaa"
 
-    
 
 class Test_Link_Olac(LanguageMixin, TestCase):
-    def test(self):
-        self.assertEqual(
-            link_olac(self.language), 
-            "http://search.language-archives.org/search.html?q=%s" % self.language.isocode
-        )
-    def test_empty(self):
-        self.assertEqual(link_olac(""), "")
+    function = staticmethod(link_olac)
+    expected = "http://search.language-archives.org/search.html?q=aaa"
 
-    def test_empty2(self):
-        self.assertEqual(link_olac(None), "")
-    
 
 class Test_Link_Multitree(LanguageMixin, TestCase):
-    def test(self):
-        self.assertEqual(
-            link_multitree(self.language), 
-            "http://multitree.org/codes/%s" % self.language.isocode
-        )
-    def test_empty(self):
-        self.assertEqual(link_multitree(""), "")
-
-    def test_empty2(self):
-        self.assertEqual(link_olac(None), "")
-    
+    function = staticmethod(link_multitree)
+    expected = "http://multitree.org/codes/aaa"
 
 
 class Test_Link_LLMap(LanguageMixin, TestCase):
-    def test(self):
-        self.assertEqual(
-            link_llmap(self.language), 
-            "http://llmap.org/languages/%s.html" % self.language.isocode
-        )
-    def test_empty(self):
-        self.assertEqual(link_llmap(""), "")
-
-    def test_empty2(self):
-        self.assertEqual(link_olac(None), "")
+    function = staticmethod(link_llmap)
+    expected = "http://llmap.org/languages/aaa.html"
 
 
 class Test_Link_Glottolog(LanguageMixin, TestCase):
-    def test(self):
-        self.assertEqual(
-            link_glottolog(self.language), 
-            "http://glottolog.org/resource/languoid/iso/%s" % self.language.isocode
-        )
-    def test_empty(self):
-        self.assertEqual(link_glottolog(""), "")
-    
-    def test_empty2(self):
-        self.assertEqual(link_olac(None), "")
+    function = staticmethod(link_glottolog)
+    expected = "http://glottolog.org/resource/languoid/iso/aaa"
+
+
+class Test_Link_Wikipedia(LanguageMixin, TestCase):
+    function = staticmethod(link_wikipedia)
+    expected = "https://en.wikipedia.org/wiki/ISO_639:aaa"
 
 
 class Test_Language_Map(LanguageMixin, TestCase):
-    def test(self):
-        self.assertEqual(
-            language_map(self.language).strip(), 
-            '<img src="http://llmap.org/language/%s.png?width=%d&height=%d" alt="Map of %s: courtesy of LL-MAP" />' %
-            (self.language.isocode, 400, 300, self.language.language)
-        )
-    def test_empty(self):
-        self.assertEqual(language_map(""), "")
-    
-    def test_empty2(self):
-        self.assertEqual(language_map(None), "")
-        
-    
+    function = staticmethod(language_map)
+    expected = '<img src="http://llmap.org/language/aaa.png?width=400&height=300" alt="Map of A: courtesy of LL-MAP" />'
+
 
