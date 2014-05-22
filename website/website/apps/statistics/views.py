55f08a34aaf5961a9ba8f4144796d06baa2546d5
Simon Greenhill <simon@simon.net.nz>
2014-05-22 15:40:42 +1000

    updating stats

--- website/website/apps/statistics/views.py
+++ website/website/apps/statistics/views.py
@@ -1,15 +1,20 @@
 from time import mktime
 from datetime import datetime
 from django.shortcuts import render
+from django.db.models import Count
 from website.apps.statistics.models import StatisticalValue
 from website.apps.statistics import statistic
+
+from website.apps.core.models import Family
+
 
 GRAPH_START_TIME = datetime.strptime("01 Apr 2013 00:00:00", "%d %b %Y %H:%M:%S")
 
 
 def format_time_struct(date):
     return int(mktime(date)) * 1000 # I have no idea why * 1000 is needed
-    
+
+
 def get_xy(label, get_latest=False):
     x = [format_time_struct(GRAPH_START_TIME.timetuple()),]
     y = [0,]
@@ -23,6 +28,20 @@ def get_xy(label, get_latest=False):
         x.append(format_time_struct(datetime.now().timetuple()))
         y.append(float(statistic.get_statistic(label)))
     return {'x': x, 'y': y, 'name': label}
+
+
+def make_family_chart():
+    x, y = [], [] 
+    for f in Family.objects.annotate(count=Count('language')):
+        x.append(f.family)
+        y.append(f.count)
+        
+    return {
+        'label': 'Language Families', 'type': "pieChart", 'extra': {},
+        'id': 'chart_id_family', 'data': {'x': x, 'y': y}, 
+    }    
+    
+    
 
 def statistics(request):
     """Shows statistics"""
@@ -41,4 +60,6 @@ def statistics(request):
             }
         })
     
+    # add pie of languages / family
+    out['family'] = make_family_chart()
     return render(request, 'statistics/details.html', out)
