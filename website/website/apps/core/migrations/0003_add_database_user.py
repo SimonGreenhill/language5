5c5d559d32f691819d8b6a70f9440ee88df86aa4
Simon Greenhill <simon@simon.net.nz>
2015-05-25 22:08:06 +0200

    oops

--- website/website/apps/core/migrations/0003_add_database_user.py
+++ website/website/apps/core/migrations/0003_add_database_user.py
@@ -7,8 +7,7 @@ from django.db import models, migrations
 def add_db_user(apps, schema_editor):
     pw = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(32))
     User = apps.get_model("auth", "User")
-    u = User.objects.create(username='mr-database')
-    u.set_password(pw)
+    u = User.objects.create(username='mr-database', password=pw)
     u.save()
 
 class Migration(migrations.Migration):
