7094e35efdd5ad1cd96e649855063fc83007cf1b
Simon Greenhill <simon@simon.net.nz>
2015-07-17 13:16:38 +1000

    updating api

--- website/website/apps/core/resources.py
+++ website/website/apps/core/resources.py
@@ -2,17 +2,24 @@ from tastypie.resources import ModelResource
 from tastypie.cache import SimpleCache
 from website.apps.core.models import Language, Source
 
-
 class LanguageResource(ModelResource):
+    
+    def determine_format(self, request):
+        return 'application/json'
+    
     class Meta:
         queryset = Language.objects.all()
         allowed_methods = ['get']
         excludes = ['comment', 'bibtex', ]
-        cache = SimpleCache(timeout=10)
+        #cache = SimpleCache(timeout=10)
         detail_uri_name = 'slug'
 
 
 class SourceResource(ModelResource):
+    
+    def determine_format(self, request):
+        return 'application/json'
+    
     class Meta:
         queryset = Source.objects.all()
         allowed_methods = ['get']
