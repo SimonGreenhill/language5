191aa6adf9c903bb57a2a4847bdd9f5d0a0c6716
Simon Greenhill <simon@simon.net.nz>
2015-09-07 14:15:18 +1000

    updating resources

--- website/website/apps/lexicon/resources.py
+++ website/website/apps/lexicon/resources.py
@@ -5,21 +5,6 @@ from tastypie.cache import SimpleCache
 from website.apps.lexicon.models import Word, Lexicon
 from website.apps.core.resources import LanguageResource, SourceResource
 
-class LexiconResource(UT8ModelResource):
-    
-    language = fields.ForeignKey(LanguageResource, 'language', full=False)
-    source = fields.ForeignKey(SourceResource, 'source', full=False)
-    
-    def determine_format(self, request):
-        return 'application/json'
-    
-    class Meta:
-        queryset = Lexicon.objects.all()
-        allowed_methods = ['get']
-        excludes = []
-        cache = SimpleCache(timeout=10)
-        authorization = DjangoAuthorization()
-
 
 class WordResource(UT8ModelResource):
     
@@ -32,3 +17,21 @@ class WordResource(UT8ModelResource):
         excludes = ['comment', 'quality', ]
         cache = SimpleCache(timeout=10)
         detail_uri_name = 'slug'
+
+
+class LexiconResource(UT8ModelResource):
+    
+    language = fields.ForeignKey(LanguageResource, 'language', full=False)
+    source = fields.ForeignKey(SourceResource, 'source', full=False)
+    word = fields.ForeignKey(WordResource, 'word', full=False)
+    
+    def determine_format(self, request):
+        return 'application/json'
+    
+    class Meta:
+        queryset = Lexicon.objects.all()
+        allowed_methods = ['get']
+        excludes = []
+        cache = SimpleCache(timeout=10)
+        authorization = DjangoAuthorization()
+
