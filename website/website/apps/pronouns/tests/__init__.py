6fd5d5acde1a4eaf3bee87dc39a66b3fdd605340
Simon Greenhill <simon@simon.net.nz>
2016-03-31 09:57:56 +1300

    updating tests

--- website/website/apps/pronouns/tests/__init__.py
+++ website/website/apps/pronouns/tests/__init__.py
@@ -5,25 +5,26 @@ from website.apps.pronouns.models import Paradigm, PronounType
 
 
 # Mixin for default test content
-class DefaultSettingsMixin(object):
-    def add_fixtures(self):
-        self.editor = User.objects.create_user(
+class PronounsTestData(object):
+    @classmethod
+    def setUpTestData(cls):
+        cls.editor = User.objects.create_user(
             'admin', 'admin@admin.com', "test"
         )
-        self.lang = Language.objects.create(
+        cls.lang = Language.objects.create(
             language='A', slug='langa',
             information='i.1',
             classification='a, b',
-            isocode='aaa', editor=self.editor
+            isocode='aaa', editor=cls.editor
         )
-        self.source = Source.objects.create(
+        cls.source = Source.objects.create(
             year="1991", author='Smith',
             slug='Smith1991', reference='S2',
-            comment='c1', editor=self.editor
+            comment='c1', editor=cls.editor
         )
-        self.word = Word.objects.create(
+        cls.word = Word.objects.create(
             word='Pronoun', slug='apronoun',
-            full='pronoun', editor=self.editor
+            full='pronoun', editor=cls.editor
         )
         
         # add some pronoun types
@@ -31,30 +32,24 @@ class DefaultSettingsMixin(object):
             w = Word.objects.create(
                 word="pronoun-a-%d-sg" % person,
                 slug="pronoun-a-%d-sg" % person,
-                editor=self.editor
+                editor=cls.editor
             )
             w.save()
             p = PronounType.objects.create(
                 word=w,
                 alignment='A', person=person, number='sg',
                 sequence=person,  # dummy variable just for sorting
-                editor=self.editor
+                editor=cls.editor
             )
             p.save()
-
-
+            
         # create this here so that _prefill_pronouns() takes advantage of our
         # newly created pronountypes
-        self.pdm = Paradigm.objects.create(language=self.lang,
-                                 source=self.source,
-                                 editor=self.editor,
-                                 comment="test")
-        self.pdm._prefill_pronouns()
+        cls.pdm = Paradigm.objects.create(
+            language=cls.lang,
+            source=cls.source,
+            editor=cls.editor,
+            comment="test"
+        )
+        cls.pdm._prefill_pronouns()
 
-
-    def test_have_some_pronoun_types(self):
-        # lots of tests will fail bizarrely if we don't have any pronountypes
-        # defined (i.e. those that iterate over PronounType._generate_all*).
-        # So here we check that the add_fixtures code has successfully set
-        # things up
-        assert PronounType.objects.count() == 3
--- website/website/apps/pronouns/tests/test_models.py
+++ website/website/apps/pronouns/tests/test_models.py
@@ -2,72 +2,86 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 
 from website.apps.core.models import Language, Source
-from website.apps.lexicon.models import Word
+from website.apps.lexicon.models import Word, Lexicon
 from website.apps.pronouns.models import Paradigm, PronounType, Pronoun
 
-class Test_PronounType(TestCase):
-    def setUp(self):
-        self.editor = User.objects.create_user(
-            'admin', 'admin@admin.com', "test"
-        )
-        self.lang = Language.objects.create(
-            language='A', slug='langa',
-            information='i.1',
-            classification='a, b',
-            isocode='aaa', editor=self.editor
-        )
-        self.source = Source.objects.create(
-            year="1991", author='Smith',
-            slug='Smith1991', reference='S2',
-            comment='c1', editor=self.editor
-        )
-        self.word = Word.objects.create(
-            word='Pronoun', slug='apronoun',
-            full='pronoun', editor=self.editor
-        )
-                                        
-        # add some pronoun types
-        for person in [1, 2, 3]:
-            w = Word.objects.create(
-                word="pronoun-a-%d-sg" % person,
-                slug="pronoun-a-%d-sg" % person,
-                editor=self.editor
-            )
-            w.save()
-            p = PronounType.objects.create(
-                word=w,
-                alignment='A', person=person, number='sg',
-                sequence=person,  # dummy
-                editor=self.editor
-            )
-            if person == 2:  # PERSON 2 should be hidden!
-                p.active = False
-            else:
-                p.active = True
-            p.save()
-    
-    def test_count(self):
-        assert PronounType.objects.count() == 2
-    
+from website.apps.pronouns.tests import PronounsTestData
+
+class Test_PronounType(PronounsTestData, TestCase):
     def test_generate_all_combinations(self):
         combinations = PronounType._generate_all_combinations()
         assert combinations[0] == PronounType.objects.filter(person=1).get()
-        assert combinations[1] == PronounType.objects.filter(person=3).get()
+        assert combinations[1] == PronounType.objects.filter(person=2).get()
+        assert combinations[2] == PronounType.objects.filter(person=3).get()
+
+
+class Test_Paradigm(PronounsTestData, TestCase):
+    def test_repr_no_label(self):
+        assert unicode(self.pdm) == 'A'
+
+    def test_repr_with_label(self):
+        self.pdm.label = 'test'
+        assert unicode(self.pdm) == 'A: test'
+        
+    def test_have_some_pronoun_types(self):
+        assert self.pdm.pronoun_set.count() == 3
     
-    def test_paradigm_create(self):
+    def test_prefill(self):
+        # make sure the correct number of pronouns is there..
+        combs = PronounType._generate_all_combinations()
+        assert self.pdm.pronoun_set.count() == len(combs)
+        
+        # check the pronouns themselves...
+        for comb in PronounType._generate_all_combinations():
+            count = Pronoun.objects.filter(pronountype=comb).count()
+            assert count == 1, 'Got %d not one' % count
+        
+    def test_partial_prefill(self):
+        # create new pdm
         pdm = Paradigm.objects.create(
             language=self.lang,
             source=self.source,
             editor=self.editor,
-            comment="test"
+            comment="test_partial_prefill"
         )
         pdm._prefill_pronouns()
         
-        # make sure the correct number of pronouns is there..
-        assert pdm.pronoun_set.count() == 2
+        # we should have a full complement.
+        combs = PronounType._generate_all_combinations()
+        assert pdm.pronoun_set.count() == len(combs)
         
-        # check the pronouns themselves...
-        for comb in PronounType._generate_all_combinations():
-            queryset = Pronoun.objects.filter(pronountype=comb)
-            assert len(queryset) == 1, 'Got {0} not one'.format(len(queryset))
+        # Let's delete some...
+        for pron in pdm.pronoun_set.all():
+            if pron.pronountype.person == '2':
+                pron.delete()
+            else:
+                # modify the stored entries so we can identify them later.
+                pron.entries.add(Lexicon.objects.create(
+                    editor=self.editor,
+                    source=self.source,
+                    language=self.lang,
+                    word=self.word,
+                    entry="old"
+                ))
+                pron.save()
+                
+        # how many should we have deleted
+        combs = PronounType._generate_all_combinations()
+        missing = [_ for _ in combs if _.person == '2']
+        assert len(missing) == 1
+        assert pdm.pronoun_set.count() == (len(combs) - len(missing))
+        
+        # re-run prefill
+        pdm._prefill_pronouns()
+        
+        # we should now have a full complement again.
+        combs = PronounType._generate_all_combinations()
+        assert pdm.pronoun_set.count() == len(combs)
+        
+        for pron in pdm.pronoun_set.all():
+            if pron.pronountype.person == '2':
+                assert pron.entries.count() == 0
+            else:
+                assert pron.entries.count() == 1
+                assert pron.entries.all()[0].entry == 'old'
         
